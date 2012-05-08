70988eae57c9b8a412483e1ae3ddc4752f155c9f
Han Jing <hanqijing@gmail.com>
2012-05-07 22:00:57 -0700

    added selector handler, twitter and fb integration

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,23 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+function selectAll(element) {
+    var text = document.getElementById(element);
+    if ($.browser.msie) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if ($.browser.mozilla || $.browser.opera) {
+        var selection = window.getSelection();
+        var range = document.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    } else if ($.browser.safari) {
+        var selection = window.getSelection();
+        selection.setBaseAndExtent(text, 0, text, 1);
+    }
+}
+
+
