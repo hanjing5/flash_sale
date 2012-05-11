0ce41b4fd276321acce536c19d0acf835b054cf2
Han Jing <hanqijing@gmail.com>
2012-05-11 11:30:57 -0700

    updated the ui for the admin console to be more informative

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -8,12 +8,19 @@ class AdminsController < ApplicationController
 			@manage_subs = 'active'
 			@title = 'Manage Subscribers'
 			@subscribers = Subscriber.all
-      @shoes_total = Subscriber.where(:shoes=>true).count
-      @watches_total = Subscriber.where(:watches=>true).count
-      @formal_ties_total = Subscriber.where(:formal_ties=>true).count
-      @denim_total = Subscriber.where(:denim=>true).count
-      @sunglasses_total = Subscriber.where(:sunglasses=>true).count
-      @casual_total = Subscriber.where(:casual=>true).count
+      @shoes_total = Subscriber.where(:shoes=>true)
+      @watches_total = Subscriber.where(:watches=>true)
+      @formal_ties_total = Subscriber.where(:formal_ties=>true)
+      @denim_total = Subscriber.where(:denim=>true)
+      @sunglasses_total = Subscriber.where(:sunglasses=>true)
+      @casual_total = Subscriber.where(:casual=>true)
+      total_list = {:shoes=>@shoes_total.count, :watches=>@watches_total.count, :formal_ties=>@formal_ties_total.count, :denim=>@denim_total.count, :sunglasses=>@sunglasses_total.count, :casual=>@casual_total.count}
+      @top3 = {}
+      while (@top3.length < 3)
+        t = total_list.max
+        @top3[t[0]] = t[1]
+        total_list.delete(total_list.max[0])
+      end
 	end
 
 	def manage_site_infos
